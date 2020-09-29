fc0f6adc08cd676c37021329fab47fe2083fe32c
VelluVu <velluv@hotmail.fi>
2020-09-29 13:58:30 +0300

    Changes to teleporting and added device tracker class
    Teleport now deactivates when not pointing the Teleport layer and after executing teleport. Teleportation Activates when trigger is pressed while pointing the teleport layer. Added InputTracker which  debug logs the current device name and adds it into static string for later use.

--- UudenmaanRuokaWebVR/Assets/Scripts/Teleport/ActivateTeleportOnHand.cs
+++ UudenmaanRuokaWebVR/Assets/Scripts/Teleport/ActivateTeleportOnHand.cs
@@ -1,62 +1,96 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.XR;
+using WebXR;
 
+/// <summary>
+/// @Author : Veli-Matti Vuoti
+/// 
+/// Changes the teleporting hand on trigger press 
+/// and 
+/// delays the teleport trigger 
+/// so 
+/// player doesn't instantly/accidently be able to teleport while aiming the Teleport area. 
+/// </summary>
 public class ActivateTeleportOnHand : MonoBehaviour
 {
-    public PhysicsRaycast[] raycasters;
-    public LineRenderer[] lines;
-
-    public float teleportingTime = 3f;
+    public bool debugging = true;
+    public TeleportRaycast[] raycasters;
+    //public LineRenderer[] lines;
+    public WebXRController[] controllers;
     public float teleportActivationDelay = 0.5f;
 
-    Coroutine teleportUptime;
     Coroutine activateTeleport;
 
-    void Update()
+    /// <summary>
+    /// Tries to automatically find the classes reguired for this to function.
+    /// </summary>
+    private void Start()
     {
-        if(raycasters[1] == null || raycasters[0] == null)
+        
+        if (raycasters[1] == null || raycasters[0] == null)
         {
-            Debug.LogError("Raycasters are not set for " + this + ". Please drag and drop the controllers into array.");
-            return;
-        }
+            raycasters[0] = transform.GetChild(0).gameObject.GetComponent<TeleportRaycast>();
+            raycasters[1] = transform.GetChild(1).gameObject.GetComponent<TeleportRaycast>();
 
-        if (!raycasters[1].active && Input.GetButtonDown("TriggerRight"))
-        {
-            if (activateTeleport != null)
-                StopCoroutine(activateTeleport);
-            if(teleportUptime != null)
-                StopCoroutine(teleportUptime);
+            if (raycasters[1] == null || raycasters[0] == null)
+            {
+                if (debugging)
+                    Debug.LogError("Raycasters are not set for " + this + ". Please drag and drop the controllers into array.");
+                return;
+            }
 
-            activateTeleport = StartCoroutine(ActivateTeleport(raycasters[1], raycasters[0], lines[0]));
-          
-            teleportUptime = StartCoroutine(TeleUp(raycasters[1], lines[1]));
-        }
+            if (controllers[0] == null || controllers[1] == null)
+            {
+                controllers[0] = raycasters[0].GetComponent<WebXRController>();
+                controllers[1] = raycasters[1].GetComponent<WebXRController>();
 
-        if(!raycasters[0].active && Input.GetButtonDown("TriggerLeft"))
-        {
-            if (activateTeleport != null)
-                StopCoroutine(activateTeleport);
-            if (teleportUptime != null)
-                StopCoroutine(teleportUptime);
-
-            activateTeleport = StartCoroutine(ActivateTeleport(raycasters[0], raycasters[1], lines[1]));
-           
-            teleportUptime = StartCoroutine(TeleUp(raycasters[0], lines[0]));
+                if (controllers[0] == null || controllers[1] == null)
+                {
+                    if (debugging)
+                        Debug.LogError("Controllers are not set for " + this + ". Please drag and drop the controllers into array.");
+                    return;
+                }
+            }
         }
     }
 
-    IEnumerator ActivateTeleport(PhysicsRaycast caster, PhysicsRaycast previousCaster, LineRenderer line)
+    /// <summary>
+    /// Shoots the ray and if collides Teleport layer player can activate the teleport hand with Trigger.
+    /// </summary>
+    void Update()
     {
-        line.positionCount = 0;
+
+        if (Physics.Raycast(raycasters[1].transform.position, raycasters[1].transform.forward, raycasters[1].maxDistance, raycasters[1].hitLayer))
+        {
+            if (!raycasters[1].active && controllers[1].GetButtonDown("Trigger"))
+            {
+                this.RestartCoroutine(ActivateTeleport(raycasters[1], raycasters[0], teleportActivationDelay), ref activateTeleport);
+            }
+        }
+        if (Physics.Raycast(raycasters[0].transform.position, raycasters[0].transform.forward, raycasters[0].maxDistance, raycasters[0].hitLayer))
+        {
+            if (!raycasters[0].active && controllers[0].GetButtonDown("Trigger"))
+            {
+                this.RestartCoroutine(ActivateTeleport(raycasters[0], raycasters[1], teleportActivationDelay), ref activateTeleport);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Deactivates the previous teleport hand and activates the new teleport hand after some time.
+    /// </summary>
+    /// <param name="caster">new hand to activate</param>
+    /// <param name="previousCaster">hand that was previously active</param>
+    /// <param name="time">time to wait till the teleport activation</param>
+    /// <returns></returns>
+    IEnumerator ActivateTeleport(TeleportRaycast caster, TeleportRaycast previousCaster, float time)
+    {
+        //line.positionCount = 0;
         previousCaster.active = false;
-        yield return new WaitForSeconds(teleportActivationDelay);
+        yield return new WaitForSeconds(time);
         caster.active = true;
     }
 
-    IEnumerator TeleUp(PhysicsRaycast caster, LineRenderer line)
-    {
-        yield return new WaitForSeconds(teleportingTime);
-        line.positionCount = 0;
-        caster.active = false;
-    }
 }
---
+++ UudenmaanRuokaWebVR/Assets/Scripts/Testing/CheckTheInputDevice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+/// <summary>
+/// @Author : Veli-Matti Vuoti
+/// 
+/// Tracks the device currently connected.
+/// </summary>
+public class CheckTheInputDevice : MonoBehaviour
+{
+
+    public InputDevice device;
+    public static string currentDevice; //Tarkista inputit? Deactivoi luokkia/gameObjecteja mitä ei voi käyttää?
+
+    private void Start()
+    {
+        device = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+        Debug.Log("CONNECTED DEVICE NAME IS : " + device.name);
+        currentDevice = device.name;
+    }
+}
